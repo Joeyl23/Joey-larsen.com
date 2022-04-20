aab35b75fb46007453c23ddcc0e5644bc65f611b
Joey Larsen <joey2003larsen@gmail.com>
2022-04-20 17:50:59 +0200

    setup react-router-dom and tailwindcss

---
+++ src/Pages/Main.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Main() {
+  return (
+    <div className='w-full min-h-screen bg-slate-600'>
+
+        <div id='MainText'>
+        Main
+        </div>
+    
+
+    </div>
+  )
+}
